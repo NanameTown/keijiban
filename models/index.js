12d03984d6fdc98f9eaa164e9c9c47e60224be1c
NanameTown <NanameTown@example.com>
2014-01-14 21:11:17 +0900

    add post

--- models/index.js
+++ models/index.js
@@ -5,3 +5,70 @@ module.exports = function(url){
 	var db = mongoose.connect(url);
 
 });
+
+var UserSchema = mongoose.Schema({
+	id:{
+		type: String,
+		index:{unique:true},
+		required:true
+	},
+	name: {
+		type: String,
+		required:true
+	}
+});
+
+var User = mongoose.model('User',UserSchema);
+
+exports.create = function(id,name,callback){
+	
+	var user = new User({
+		id:id,
+		name:name
+	});
+
+	user.save(function(err,user){
+		if(err){
+			console.error(err);
+		}
+		callback && callback(err,user);
+	});
+};
+
+exports.read = function(id,callback){
+	User.findOne(function(err,users){
+		if(err){
+			console.error(err);
+		}
+		callback(err,users);
+	});
+};
+
+exports.update = function(id,name,callback){
+	User.findOne({id:id},function(err,user){
+		if(err){
+			console.error(err);
+		}
+		user.name = name;
+		user.save(function(err,user){
+			if(err){
+				console.error(err);
+			}
+			callback && callback(err,user);
+		});
+	});
+};
+
+exports.delete = function(id,callback){
+	User.findOne({id:id},function(err,user){
+		if(err){
+			console.errer(err);
+		}
+		user.remove(function(err){
+			if(err){
+				console.error(err);
+			}
+			callback && callback(err);
+		});
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,29 @@ module.exports = function(){
 
 	this.get("/",function(req,res){
 	
-		res.render("index");
+		res.render("index",{ title:"一覧"});
+	
+	});
+	this.post("/",function(req,res){
+		res.render("index",{});
+	
+	});
+
+	this.get("/new",function(req,res){
+
+		res.render("index", {});
+	
+	});
+	var posts = [];
+
+	this.post("/new",function(req,res){
+		if(req.body.name && req.body.title && req.body.remark){
+			posts.push(req.body.name);
+			console.log(req.body.name);
+			res.render("index",{name:posts});
+		}else{
+			
+		}
 	
 	});
 
